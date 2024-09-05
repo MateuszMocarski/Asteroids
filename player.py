e703aa11ccf4ed571fe72c80f7c219cc73befe65
MateuszMocarski <mat.mocarski@gmail.com>
2024-09-05 23:37:08 +0200

    Order of methods fixed

--- player.py
+++ player.py
@@ -22,6 +22,10 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += constants.PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * constants.PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -34,6 +38,3 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
     
-    def move(self, dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * constants.PLAYER_SPEED * dt
