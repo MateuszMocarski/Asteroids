5db823a46d18f8a6f4edcf05818c522ae827992d
MateuszMocarski <mat.mocarski@gmail.com>
2024-09-05 19:29:23 +0200

    removal of redundant data from fill method

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill("black", rect=None, special_flags=0)
+        screen.fill("black")
         pygame.display.flip()
 
 if __name__ == "__main__":
